fc4f7cbcac275d263f3356e2cc901e676b97c257
death-shadow <siddharthpaul2006@gmail.com>
2020-10-02 11:38:50 +0530

    array break

---
+++ Codechef-DSA-Series/ccpc1/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+	int index,a[2000];
+	int sum=0;
+
+	
+	for(int i=0;i<2000;i++){
+		cin>>a[i];
+
+		
+
+if(a[i]==42){
+index=i;
+
+	break;
+}
+}
+for(int j=0;j<index;j++)
+{
+	cout<<a[j]<<endl;
+
+	}
+    
+
+
+}
+
